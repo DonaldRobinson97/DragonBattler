d167ecce8c2e0a9285f00e2b6b6c36d4ec1327e5
DonaldRobinson97 <donaldrobinson1010@gmail.com>
2025-03-18 00:16:34 +0530

    Player Movement Tweak

--- Assets/Game Assets/Scripts/Player/PlayerController.cs
+++ Assets/Game Assets/Scripts/Player/PlayerController.cs
@@ -1,29 +1,40 @@
 using DG.Tweening;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float moveSpeed = 10f;
-    [SerializeField] private float lookDuration = 10f;
+    [SerializeField] private float rotationSpeed = 10f;
     [SerializeField] private Animator _animator;
     private Tweener moveTweener;
     [SerializeField] private string EnemyTag;
 
+    [Header("Position Marker")]
+    public LayerMask groundLayer;
+    [SerializeField] private ParticleSystem markerObject;
+
     #region Unity
     private void OnEnable()
     {
-        EventController.StartListening(GameEvent.EVENT_POSITION_MARKED, OnPositionMarked);
     }
 
     private void Start()
     {
         moveTweener.SetAutoKill();
+        markerObject.transform.position = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        ClickTarget();
+        FaceTarget();
     }
 
     private void OnDisable()
     {
-        EventController.StopListening(GameEvent.EVENT_POSITION_MARKED, OnPositionMarked);
     }
 
 
@@ -48,22 +59,40 @@ public class PlayerController : MonoBehaviour
         float distance = Vector3.Distance(Pos, rb.transform.position);
         float duration = distance / moveSpeed;
 
-        float angleDiff = Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, GetLookAtYRotation(transform.position, Pos)));
-        float turnDuration = angleDiff / lookDuration;
-
-        this.transform.DOLookAt(Pos, turnDuration).OnComplete(
+        _animator.SetBool("Walk", true);
+        moveTweener = rb.DOMove(Pos, duration).OnComplete(
             () =>
             {
-                _animator.SetBool("Walk", true);
-                moveTweener = rb.DOMove(Pos, duration).OnComplete(
-                    () =>
-                    {
-                        _animator.SetBool("Walk", false);
-                    }
-                );
+                _animator.SetBool("Walk", false);
             }
-
         );
+
+    }
+
+    private void FaceTarget()
+    {
+        if (rb.transform.position != markerObject.transform.position)
+        {
+            Vector3 direction = (markerObject.transform.position - rb.transform.position).normalized;
+            Quaternion rotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+            rb.rotation = Quaternion.Slerp(rb.rotation, rotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    private void ClickTarget()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 500f, groundLayer))
+            {
+                markerObject.transform.position = hit.point;
+                markerObject.Play();
+                MovePlayer(hit.point);
+            }
+        }
     }
 
     private void stopWalk()
@@ -81,16 +110,6 @@ public class PlayerController : MonoBehaviour
         direction.y = 0;
 
         return Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-    }
-    #endregion
-
-    #region Callbacks
-    private void OnPositionMarked(object Args)
-    {
-        if (Args != null)
-        {
-            MovePlayer((Vector3)Args);
-        }
     }
     #endregion
 }
